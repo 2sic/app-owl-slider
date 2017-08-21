52b706dab929248bdf6c73ed3eaff1eb5408abd4
lucatescari <luca@ltescari.ch>
2017-08-21 14:41:22 +0200

    Adjusted gulpfile and cleaned up unneeded files

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,14 @@
 
 var bases = {
     owl: "bower_components/owlcarousel/owl-carousel/",
-    src: "src/",
-    distLib: "dist/lib/owlcarousel/",
-    distApp: "dist/app/"
+    src: "assets/",
+    distLib: "dist/lib/owl2/assets/"
 };
 
 
-gulp.task("import-library", function () {
-    gulp.src([bases.owl + "owl.carousel.min.js"]) 
-        .pipe(concat('owl-set.js'))
-        .pipe(gulp.dest(bases.distLib));
-
-    gulp.src(bases.owl + "owl.carousel.css")
-        .pipe(concat("owl-set.css"))
-        .pipe(gulp.dest(bases.distLib));
-
-    gulp.src(bases.owl + "*.@(png|gif)")
-        .pipe(gulp.dest(bases.distLib));
-});
-
 gulp.task("build-css", function() {
     gulp.src(bases.src + "*.css")
-        .pipe(gulp.dest(bases.distApp));
+        .pipe(gulp.dest(bases.distLib));
 });
 
 gulp.task("default", function() {
