4b8cafe1c989d4a48594ae5c9b47e5c27a2d6a08
lucatescari <luca@ltescari.ch>
2017-08-21 11:32:03 +0200

    First working commit with owl v2

---
+++ README.md
@@ -0,0 +1,2 @@
+# app-owl-carousel for DNN (DotNetNuke)
+A 2sxc / DNN App based on [OWL carousel slider](http://owlgraphic.com/owlcarousel/)
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+﻿var gulp = require('gulp'),
+    concat = require("gulp-concat");
+
+var bases = {
+    owl: "bower_components/owlcarousel/owl-carousel/",
+    src: "src/",
+    distLib: "dist/lib/owlcarousel/",
+    distApp: "dist/app/"
+};
+
+
+gulp.task("import-library", function () {
+    gulp.src([bases.owl + "owl.carousel.min.js"]) 
+        .pipe(concat('owl-set.js'))
+        .pipe(gulp.dest(bases.distLib));
+
+    gulp.src(bases.owl + "owl.carousel.css")
+        .pipe(concat("owl-set.css"))
+        .pipe(gulp.dest(bases.distLib));
+
+    gulp.src(bases.owl + "*.@(png|gif)")
+        .pipe(gulp.dest(bases.distLib));
+});
+
+gulp.task("build-css", function() {
+    gulp.src(bases.src + "*.css")
+        .pipe(gulp.dest(bases.distApp));
+});
+
+gulp.task("default", function() {
+    gulp.watch(bases.src + "*.*", ["build-css"]);
+});
