e00fc6313eaab3d80ed891f6d04e0cf768a92b12
lucatescari <luca@ltescari.ch>
2018-08-15 08:41:44 +0200

    enabled minifying of css

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ gulp.task('watch-sass-all', function () {
 gulp.task('sass-all', function () {
   return gulp.src(['./src/scss/*.scss'])
 	.pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 	.pipe(sourcemaps.write('.', { sourceRoot: '../src/scss/' }))
     .pipe(gulp.dest('./dist/'));
 });
@@ -20,7 +20,7 @@ gulp.task('watch-sass-bs3', function () {
 gulp.task('sass-bs3', function () {
   return gulp.src(['./src/scss/bs3.scss'])
 	.pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 	.pipe(sourcemaps.write('.', { sourceRoot: '../src/scss/' }))
     .pipe(gulp.dest('./dist/'));
 });
@@ -32,7 +32,7 @@ gulp.task('watch-sass-bs4', function () {
 gulp.task('sass-bs4', function () {
   return gulp.src(['./src/scss/bs4.scss'])
 	.pipe(sourcemaps.init())
-	.pipe(sass().on('error', sass.logError))
+	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 	.pipe(sourcemaps.write('.', { sourceRoot: '../src/scss/' }))
 	.pipe(gulp.dest('./dist/'));
 });
